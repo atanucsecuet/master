dec0155ea14a7c15014ff000ec6f9b4657de8391
Atanu Saha <atanucsecuet@gmail.com>
2015-10-03 01:12:47 +0600

    add initial project | start

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,11 @@
+<div class="navbar">
+    <div class="navbar-inner">
+        <a id="logo" href="{{URL::to('/')}}">Single Malt</a>
+        <ul class="nav">
+            <li><a href="{{URL::to('/')}}">Home</a></li>
+            <li><a href="{{URL::to('/about')}}">About</a></li>
+            <li><a href="{{URL::to('/projects')}}">Projects</a></li>
+            <li><a href="{{URL::to('/contact')}}">Contact</a></li>
+        </ul>
+    </div>
+</div>
